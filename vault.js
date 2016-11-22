179086a3a390364a5af8881a3b916725fbd404be
Andrea <takamiya.andrea@gmail.com>
2016-11-21 22:36:04 -1000

    All tests passed! Had to walk through the steps to follow when each of these functions were triggered. Added comments as I traced things

--- vault.js
+++ vault.js
@@ -1,26 +1,28 @@
 'use strict';
 module.exports = function () {
 
+// Create variables to store the value and key across both setValue and getValue functions
   var secretValue = 0;
+  var secretKey = 0;
 
   var setValue = function (key, value) {
-    key = secretValue;
-    return getValue();
+    secretValue = value;  // Set the secretValue variable to hold the value
+    secretKey = key;  // Set the secretKey variable to hold the key
+    return getValue();  // Trigger the getValue function since the variables have been set
   };
 
 
   var getValue = function (key) {
-    if (!key) {
-        return null;
+    if (key === null) {   // If there is no key, return null
+      return null;
     }
-    else if (key === setValue) {
-      return setValue;
+    else if (key === secretKey) {  // Use the key set in the setValue function to return the secretValue
+      return secretValue;
     }
     else {
-      return setValue();
+      return null;  // If the key doesn't match the key that was set in the setValue function, return null
     }
   };
-
 
   return {
     setValue: setValue,
