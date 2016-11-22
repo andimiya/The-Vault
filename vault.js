77e9f17b5fd1a8d2b9ee674f430782386e0f18af
Andrea <takamiya.andrea@gmail.com>
2016-11-21 21:33:56 -1000

    Passed test if an invalid key is used

--- vault.js
+++ vault.js
@@ -2,14 +2,23 @@
 module.exports = function () {
 
   var setValue = function (key, value) {
-
-    // return value;
+    value = 'hunter2';
+    return value;
   };
 
   var getValue = function (key) {
-    if (!key) {
+    if (key === null) {
+        return null;
+    }
+
+    else if (key === 'secret password') {
+      return setValue();
+    }
+
+    else {
       return null;
     }
+
   };
 
   return {
@@ -18,14 +27,3 @@ module.exports = function () {
   };
 
 };
-
-
-// var myVault = vault();
-
-// console.log( myVault.getValue() ); // null
-// console.log( myVault.setValue('secret password', 'hunter2') );
-// console.log( myVault.getValue('secret password') ); // hunter2
-// console.log( myVault.getValue('bank account') ); // null
-
-// var yourVault = vault();
-// console.log( myVault.getValue('secret password') ); // null
