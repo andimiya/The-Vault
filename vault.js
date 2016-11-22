678377c762c0bc8258246de2af201afc2e4003a9
Andrea <takamiya.andrea@gmail.com>
2016-11-21 19:47:08 -1000

    Initial setup of functions with nothing in them

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function () {
+
+  function setValue(key, value) {
+
+    return value;
+  }
+
+  function getValue(key) {
+
+    return key;
+  }
+
+
 };
+
+
+// var myVault = vault();
+
+// console.log( myVault.getValue() ); // null
+// console.log( myVault.setValue('secret password', 'hunter2') );
+// console.log( myVault.getValue('secret password') ); // hunter2
+// console.log( myVault.getValue('bank account') ); // null
+
+// var yourVault = vault();
+// console.log( myVault.getValue('secret password') ); // null
